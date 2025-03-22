55f22cf2f33b0c52652c391dbbeb02f75f023859
marisel <140407389+betingrich@users.noreply.github.com>
2025-03-23 00:58:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-document.addEventListener('scroll', function(event) {
+document.addEventListener('DOMContentLoaded', function() {
     const slides = document.querySelectorAll('.slide');
+
     slides.forEach(slide => {
-        const rect = slide.getBoundingClientRect();
-        if (rect.left >= 0 && rect.right <= window.innerWidth) {
-            slide.style.transform = 'scale(1.1)';
-        } else {
-            slide.style.transform = 'scale(0.9)';
-        }
+        slide.addEventListener('click', function() {
+            const profileId = slide.id;
+            window.location.href = `${profileId}.html`;
+        });
+    });
+
+    document.addEventListener('scroll', function(event) {
+        slides.forEach(slide => {
+            const rect = slide.getBoundingClientRect();
+            if (rect.left >= 0 && rect.right <= window.innerWidth) {
+                slide.style.transform = 'scale(1.1)';
+            } else {
+                slide.style.transform = 'scale(0.9)';
+            }
+        });
     });
 });
